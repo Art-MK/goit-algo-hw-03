4f48c5f0a99492722756b107dff31dd83fc7bb24
Art-MK <54836473+Art-MK@users.noreply.github.com>
2024-03-06 21:55:18 +0200

    fix  comparison 2v

--- m3_2.py
+++ m3_2.py
@@ -16,7 +16,7 @@ def get_numbers_ticket(min_val, max_val, quantity) -> set:
             raise ValueError("Аргументи функції повинні бути цілими числами.")
 
         # Перевірка коректності деапозона вхідних даних
-        if not (1 <= min_val <= max_val <= 1000) or not (min_val >= quantity <= max_val):
+        if not (1 <= min_val <= max_val <= 1000) or not (quantity >= max_val - min_val + 1):
             raise ValueError("Некоректні параметри функції. Перевірте діапазон значень")
 
         # Генерування унікальних випадкових чисел у заданому діапазоні
